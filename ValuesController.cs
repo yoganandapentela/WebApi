f04409e66bd8b20efbee7de941f8b45a15215df2
yoganandapentela <117732016+yoganandapentela@users.noreply.github.com>
2023-10-05 18:44:00 +0530

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Configuration;
+using System.Data;
+
+namespace WebApplication1.Controllers
+{
+   
+    public class ValuesController : ApiController
+    {
+       YogaEmployee emp = new YogaEmployee();
+
+        // GET api/values
+        public List<YogaEmployee> Get()
+        {
+            List<YogaEmployee> employees = new List<YogaEmployee>(); 
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["YogaEmp"].ConnectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter("select * from YogaEmployee", con);
+                DataTable dt = new DataTable();
+
+                adapter.Fill(dt);
+
+                for(int i=0;i<dt.Rows.Count;i++)
+                {
+                    YogaEmployee emp = new YogaEmployee();
+                    emp.Id =Convert.ToInt32(dt.Rows[i]["Id"]);
+                    emp.Name = dt.Rows[i]["Name"].ToString();
+                    emp.Age = Convert.ToInt32(dt.Rows[i]["Age"]);
+                    employees.Add(emp);
+
+                }
+
+            }
+            return employees;
+        }
+
+        // GET api/values/5
+        public YogaEmployee Get(int id)
+
+        { 
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["YogaEmp"].ConnectionString))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("select * from YogaEmployee where Id=@Id", con);
+
+                command.Parameters.AddWithValue("@Id",id);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                
+           /*      SqlDataAdapter adapter= new SqlDataAdapter("select * from YogaEmployee where Id=@Id", con);
+                adapter.SelectCommand.Parameters.AddWithValue("@Id", id);*/
+
+                 
+
+
+                DataTable dt = new DataTable();
+                 adapter.Fill(dt);
+                 if (dt.Rows.Count > 0)
+                {
+
+                    emp.Id = Convert.ToInt32(dt.Rows[0]["Id"]);
+                    emp.Name = dt.Rows[0]["Name"].ToString();
+                    emp.Age = Convert.ToInt32(dt.Rows[0]["Age"]);
+                }
+                
+
+            }
+            return emp;
+           
+        }
+
+        // POST api/values
+        public string Post(YogaEmployee emp)
+        {
+            string response;
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["YogaEmp"].ConnectionString))
+            {
+                con.Open();
+                SqlCommand command = new SqlCommand("Insert into YogaEmployee(Id,Name,Age) values (@Id,@Name,@Age)", con);
+
+                command.Parameters.AddWithValue("@Id", emp.Id);
+                command.Parameters.AddWithValue("@Name", emp.Name);
+                command.Parameters.AddWithValue("@Age", emp.Age);
+
+                //SqlDataAdapter adapter = new SqlDataAdapter(command);
+                int i = command.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    response = "success";
+                }
+                else
+                {
+                    response = "Fail";
+                }
+                return response;
+
+            }
+                
+          }
+
+        // PUT api/values/5
+        public void Put(int id,YogaEmployee emp)
+        {
+           
+            if (emp!=null)
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["YogaEmp"].ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand command = new SqlCommand("update YogaEmployee set Name=@Name, Age=@Age where Id=@Id", con);
+
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@Name", emp.Name);
+                    command.Parameters.AddWithValue("@Age", emp.Age);
+
+                    //SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    int i = command.ExecuteNonQuery();
+                   
+                }
+                
+            }
+           
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            if (emp != null)
+            {
+                using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["YogaEmp"].ConnectionString))
+                {
+                    con.Open();
+                    SqlCommand command = new SqlCommand("Delete from YogaEmployee where Id=@Id", con);
+
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    //SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    int i = command.ExecuteNonQuery();
+
+
+                }
+            }
+        }
+    
+    }
+}
---
+++ WebApi.js
@@ -0,0 +1,173 @@
+﻿    var selectedRow = null;
+    displaydata();
+    function noRecordsFound() {
+        if ($("#employeeList tbody").find("tr").length == 0) {
+            $("#norecords").text("NO RECORDS FOUND");
+        }
+        else {
+            $("#norecords").text("");
+
+        }
+    }
+    noRecordsFound();
+    
+    //Main function...
+    function formsubmit() {
+        if (validate()) {
+             var formdata = fun1();
+            if (selectedRow == null) {
+                $("#norecords").text('');
+               
+                insertnewrecord(formdata);
+
+            }
+            else {
+                updatenewrecord(formdata);
+            }
+            //resetform();
+        }
+
+    }
+
+    function fun1() {
+        formdata = {};
+        formdata["Id"] = $("#Id").val();
+        formdata["Name"] = $("#Name").val();
+        formdata["Age"] = $("#Age").val();
+
+        return formdata;
+    }
+//function to insert a new record
+    function insertnewrecord(formdata) {
+        $.ajax({
+            url: '/api/Values',
+            contentType: 'application/json ;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(formdata),
+            type: 'POST',
+            success: function (result) {
+                //alert(result);
+                displaydata();
+            },
+            error: function () {
+                alert("no data inserted");
+            }
+
+        }
+        )
+
+
+        resetform();
+
+    }
+
+//function to update record
+    function updatenewrecord(formdata) {
+        let id = formdata.Id;
+        $.ajax({
+            url: '/api/Values/'+id,
+            contentType: 'application/json ;charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(formdata),
+            type: 'PUT',
+            success: function (result) {
+                //alert(result);
+                displaydata();
+            },
+            error: function () {
+                alert("no data inserted");
+            }
+
+        }
+        )
+        $("#submit").prop("value", "submit");
+
+        resetform();
+
+    }
+    //function to display data
+    function displaydata() {
+        $.ajax({
+            url: '/api/Values',
+            contentType: 'application/json ;charset=utf-8',
+            dataType: 'json',
+            type: 'GET',
+            success: function (result) {
+                if (result) {
+                    $("#employeeList tbody").html('');
+                    var row = '';
+                    for (let i = 0; i < result.length; i++) {
+                        row = row + '<tr>' + '<td>' + result[i].Id + '</td>' +
+                            '<td>' + result[i].Name + '</td>' +
+                            '<td>' + result[i].Age + '</td>' +
+                            '<td><button onClick="onEdit(this)">Edit</button><button onClick="onDelete(' + result[i].Id + ')">Delete</button></td>' + '</tr>';
+
+                    }
+                    if (row != '') {
+                        $("#employeeList tbody").append(row);
+
+                    }
+                }
+            },
+            error: function () {
+                alert("no data inserted");
+            }
+
+        }
+        )
+    }
+    
+    //function to delete
+    function onDelete(id) {
+        $.ajax({
+            url: '/api/Values/' + id,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            type: 'DELETE',
+            success: function () {
+                alert("deleted");
+                noRecordsFound();
+
+                displaydata();
+                
+            },
+            error: function () {
+                alert("data not deleted");
+            }
+
+
+        })
+    }
+
+//function to edit
+    function onEdit(td) {
+        selectedRow = $(td).closest("tr"); // Get the closest table row (tr) to the clicked button
+        $("#Id").val(selectedRow.find("td:eq(0)").text());
+        $("#Name").val(selectedRow.find("td:eq(1)").text());
+        $("#Age").val(selectedRow.find("td:eq(2)").text());
+        $("#submit").prop("value", "Update");
+
+
+    }
+
+//function to reset
+    function resetform() {
+        $("#Id").val("");
+        $("#Name").val("");
+        $("#Age").val("");
+        selectedRow = null;
+    }
+//function to serach
+    $("#searchbar").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#employeeList tr").filter(function () {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+//function for validation
+    function validate() {
+        return true;
+    }
+
+
+    
